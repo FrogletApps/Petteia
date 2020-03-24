9df5b6e431ab1da590118570cccae8d46dfb65f4
James Pearson <james_pearson@outlook.com>
2020-03-24 17:25:50 +0000

    Add some collision detection
    I wrote this code a while back and I must have forgotten to push it, I can't remember exactly what it does but it seems to be collision detection and it seems to work so hey

--- petteia.js
+++ petteia.js
@@ -28,7 +28,7 @@ function setupBoard(boardX, boardY){
         else{
             for (var j = 0; j < board[i].length; j++) {
                 //console.log("j = " + j)
-                board[i][j] = null;
+                board[i][j] = 0;
             }
             
         }
@@ -57,14 +57,14 @@ function move(xStartPos, yStartPos, xEndPos, yEndPos){
     if (checkRules(xStartPos, yStartPos, xEndPos, yEndPos)){
         activePiece = board[yStartPos][xStartPos];
         board[yEndPos][xEndPos] = activePiece;
-        board[yStartPos][xStartPos] = null;
+        board[yStartPos][xStartPos] = 0;
+        takePieces(xEndPos, yEndPos);
     }
-
-    takePieces(xEndPos, yEndPos);
 
     makeTableHTML(board);
 }
 
+//Makes sure a move is valid
 function checkRules(xStartPos, yStartPos, xEndPos, yEndPos){
     if (!board[yStartPos][xStartPos]){
         alert("No piece selected");
@@ -78,46 +78,106 @@ function checkRules(xStartPos, yStartPos, xEndPos, yEndPos){
         alert("Destination position is already taken");
         return false;
     }
+    //TODO check for out of array
+    //TODO check for not jumping over any pieces
     else{
         return true;
     }
 }
 
-function takePieces(xEndPos, yEndPos){
-    console.log("N " + getAdjacent(xEndPos, yEndPos, "N").length)
-    console.log("E " + getAdjacent(xEndPos, yEndPos, "E").length)
-    console.log("S " + getAdjacent(xEndPos, yEndPos, "S").length)
-    console.log("W " + getAdjacent(xEndPos, yEndPos, "W").length)
+function takePieces(xPos, yPos){
+    var team = board[yPos][xPos];
 
-    var team = board[yEndPos][xEndPos];
+    var northAdjacent = getAdjacent(xPos, yPos, "N");
+    var eastAdjacent = getAdjacent(xPos, yPos, "E");
+    var southAdjacent = getAdjacent(xPos, yPos, "S");
+    var westAdjacent = getAdjacent(xPos, yPos, "W");
 
-    var northAdjacent = getAdjacent(xEndPos, yEndPos, "N");
-    var eastAdjacent = getAdjacent(xEndPos, yEndPos, "E");
-    var southAdjacent = getAdjacent(xEndPos, yEndPos, "S");
-    var westAdjacent = getAdjacent(xEndPos, yEndPos, "W");
+    console.log("N adjacent = " + northAdjacent.length)
+    console.log("E adjacent = " + eastAdjacent.length)
+    console.log("S adjacent = " + southAdjacent.length)
+    console.log("W adjacent = " + westAdjacent.length)
 
     if (northAdjacent.length > 0){
+        console.log("Looking to the north")
         var northTake = findPiecesToTake(northAdjacent, team);
-        for (var i = northTake.length - 1; i >= 0; i--){
-            //TODO - Add code for taking pieces
+        console.log("northTake = " + northTake);
+        if (northTake[0]){
+            console.log("Taking pieces to the north")
+            //for (var i = 0; i < northTake[1].length; i++){
+            for (var i = northTake[1].length; i >= 0 ; i--){
+                console.log("Replace " + northTake[1][i] + " at " + xPos+i + "," + yPos);
+                board[yPos-i][xPos] = northTake[1][i];
+            }
         }
         console.log(northTake);
     }
+
+    if (eastAdjacent.length > 0){
+        console.log("Looking to the east")
+        var eastTake = findPiecesToTake(eastAdjacent, team);
+        console.log("eastTake = " + eastTake);
+        if (eastTake[0]){
+            console.log("Taking pieces to the east")
+            //for (var i = 0; i < eastTake[1].length; i++){
+            for (var i = eastTake[1].length; i >= 0 ; i--){
+                    console.log("Replace " + eastTake[1][i] + " at " + xPos+i + "," + yPos);
+                    board[yPos][xPos+i] = eastTake[1][i];
+            }
+        }
+        console.log(eastTake);
+    }
+
+    if (southAdjacent.length > 0){
+        console.log("Looking to the south")
+        var southTake = findPiecesToTake(southAdjacent, team);
+        console.log("southTake = " + southTake);
+        if (southTake[0]){
+            console.log("Taking pieces to the south")
+            //for (var i = 0; i < southTake[1].length; i++){
+            for (var i = southTake[1].length; i >= 0 ; i--){
+                console.log("Replace " + southTake[1][i] + " at " + xPos+i + "," + yPos);
+                board[yPos+i][xPos] = southTake[1][i];
+            }
+        }
+        console.log(southTake);
+    }
+    if (westAdjacent.length > 0){
+        console.log("Looking to the west")
+        var westTake = findPiecesToTake(westAdjacent, team);
+        console.log("westTake = " + westTake);
+        if (westTake[0]){
+            console.log("Taking pieces to the west")
+           // for (var i = 0; i < westTake[1].length; i++){
+            for (var i = westTake[1].length; i >= 0 ; i--){
+                console.log("Replace " + westTake[1][i] + " at x:" + xPos+i + ", y:" + yPos);
+                board[yPos][xPos-i] = westTake[1][i];
+            }
+        }
+        console.log(westTake);
+    }
 }
 
+//Returns an array of adjacent pieces with the correct ones taken
 function findPiecesToTake(adjacent, team){
-    var result = 0;
     var captured = false;
-    for (var i = adjacent.length - 1; i >= 0; i--) {
-        console.log(i + " - " + adjacent[i]);
-        if (!captured && adjacent[i] == team){
-            captured = true;
-        }
-        if (captured && adjacent[i] != team){
-            adjacent[i] = null;
+    var result = new Array();
+    //If both teams are involved 
+    if (adjacent.includes(1, 2)){
+        for (var i = adjacent.length - 1; i >= 0; i--) {
+            console.log(i + " - " + adjacent[i]);
+            //If not already captured, and it's not the next tile and they are your team then you can capture some tiles
+            if (!captured && adjacent[i] == team){
+                captured = true;
+            }
+            if (captured && adjacent[i] != team){
+                adjacent[i] = 0;
+            }
         }
     }
-    return adjacent;
+    result.push(captured);
+    result.push(adjacent);
+    return result;
 }
 
 //Get the adjacent pieces
@@ -136,16 +196,16 @@ function getAdjacent(xPos, yPos, direction, pieces){
                 pieces.push(board[yPos][xPos]);
                 result = getAdjacent(xPos, yPos, direction, pieces);
             }
-
         break;
+
         case "E":
             xPos++;
             if (xPos < board[0].length && board[yPos][xPos] ){
                 pieces.push(board[yPos][xPos]);
                 result = getAdjacent(xPos, yPos, direction, pieces);
             }
-
         break;
+
         case "S":
             yPos++;
             if (yPos < board.length && board[yPos][xPos]){
@@ -153,6 +213,7 @@ function getAdjacent(xPos, yPos, direction, pieces){
                 result = getAdjacent(xPos, yPos, direction, pieces);
             }
         break;
+
         case "W":
             xPos--;
             if (xPos >= 0 && board[yPos][xPos] ){
@@ -160,20 +221,23 @@ function getAdjacent(xPos, yPos, direction, pieces){
                 result = getAdjacent(xPos, yPos, direction, pieces);
             }
         break;
+
         default:
             console.log("Invalid direction " + direction);
         break;
     }
+    console.log(pieces.length + " pieces in " + direction);
     return result;
 }
 
+//Turns an array into a HTML table
 //Adapted from https://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
 function makeTableHTML(myArray) {
     var result = "<table border=1>";
     for(var i=0; i<myArray.length; i++) {
         result += "<tr>";
         for(var j=0; j<myArray[i].length; j++){
-            if (myArray[i][j]){
+            if (myArray[i][j] != 0){
                 result += "<td>"+myArray[i][j]+"</td>";
             }
             else{
